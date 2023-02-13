b81aca543f20e8b43ec4dc89fb92acdfac848b8b
LEEHYUNBOK <gusqhrzkq@naver.com>
2023-02-13 13:18:35 +0900

    Fix : login passwordEncoder 2번작용 수정

--- src/main/java/com/example/developers/service/JwtUserDetailsService.java
+++ src/main/java/com/example/developers/service/JwtUserDetailsService.java
@@ -28,7 +28,7 @@ public class JwtUserDetailsService implements UserDetailsService {
     private UserDetails createUserDetails(Member member){
         return User.builder()
                 .username(member.getUsername())
-                .password(passwordEncoder.encode(member.getPassword()))
+                .password(member.getPassword())
                 .roles(member.getRoles().toArray(new String[0]))
                 .build();
     }
