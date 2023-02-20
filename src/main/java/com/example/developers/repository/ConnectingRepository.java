42a867e80f3f87cecbe751d5c994b8043df30796
LEEHYUNBOK <gusqhrzkq@naver.com>
2023-02-21 03:37:11 +0900

    Fix(#2) : Connecting findByStore 반환값 entity로 변경

--- src/main/java/com/example/developers/repository/ConnectingRepository.java
+++ src/main/java/com/example/developers/repository/ConnectingRepository.java
@@ -1,9 +1,8 @@
 package com.example.developers.repository;
 
-import com.example.developers.DTO.ConnectingDTO;
 import com.example.developers.domain.Connecting;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ConnectingRepository extends JpaRepository<Connecting, Long> {
-    ConnectingDTO findByStore(String store);
+    Connecting findByStore(String store);
 }
