e3d67e0a389b0a3e2f9b4e01057b6f9585f236da
LEEHYUNBOK <gusqhrzkq@naver.com>
2023-02-21 03:54:30 +0900

    Fix(#2) : 가게 찾기 시 1 이상일 경우로 변경

--- src/main/java/com/example/developers/repository/ConnectingRepository.java
+++ src/main/java/com/example/developers/repository/ConnectingRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ConnectingRepository extends JpaRepository<Connecting, Long> {
-    Connecting findByStore(String store);
+    List<Connecting> findByStore(String store);
     List<Connecting> findByTag(String tag);
 }
