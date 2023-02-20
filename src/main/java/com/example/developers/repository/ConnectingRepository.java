1c381f3e7550e02d479390c02663d30a1c0d5224
LEEHYUNBOK <gusqhrzkq@naver.com>
2023-02-21 03:40:14 +0900

    Feat(#2) : connecting tag 찾기 추가

--- src/main/java/com/example/developers/repository/ConnectingRepository.java
+++ src/main/java/com/example/developers/repository/ConnectingRepository.java
@@ -3,6 +3,9 @@ package com.example.developers.repository;
 import com.example.developers.domain.Connecting;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ConnectingRepository extends JpaRepository<Connecting, Long> {
     Connecting findByStore(String store);
+    List<Connecting> findByTag(String tag);
 }
