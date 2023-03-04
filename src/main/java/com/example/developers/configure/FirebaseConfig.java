a12f65582dc5ef1c005bcc97ff83b567747798cc
LEEHYUNBOK <gusqhrzkq@naver.com>
2023-03-04 21:37:16 +0900

    Feat(#1) : yml에 키 저장

--- src/main/java/com/example/developers/configure/FirebaseConfig.java
+++ src/main/java/com/example/developers/configure/FirebaseConfig.java
@@ -4,6 +4,7 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.FirebaseAuth;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,11 +13,14 @@ import java.io.IOException;
 
 @Configuration
 public class FirebaseConfig {
+
+    @Value("${spring.firebase.key}")
+    private String firebaseKey;
     @Bean
     public FirebaseAuth firebaseAuth() throws IOException {
         System.out.println("Initializing Firebase.");
         FileInputStream serviceAccount =
-                new FileInputStream("../firebase.json");
+                new FileInputStream(firebaseKey);
 
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
