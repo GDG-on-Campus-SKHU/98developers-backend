81c1bc08aefe6aedb9236a5c0c9840e0c82a4ef6
LEEHYUNBOK <gusqhrzkq@naver.com>
2023-03-07 16:37:04 +0900

    Modi(#3) : 변수 명 피드백 적용

--- src/main/java/com/example/developers/DTO/ChallengeDTO.java
+++ src/main/java/com/example/developers/DTO/ChallengeDTO.java
@@ -1,6 +1,8 @@
 package com.example.developers.DTO;
 
+import com.sun.istack.NotNull;
 import lombok.*;
+import org.springframework.lang.Nullable;
 
 import java.util.Date;
 
@@ -13,14 +15,20 @@ public class ChallengeDTO {
 
     private Long id;
 
-    private String goal;
+    private String topic; // 주제
 
-    private String authMethod;
+    private String howProof; // 인증 방법
 
-    private String expectedEffects;
+    private String expectedResults; // 기대 효과
 
-    private String caution;
+    private String pleaseNote; // 주의 사항
 
-    private Date date;
+    private String collectProof; // 올바른 예시 이미지
+
+    private Date periodStartDate; // 인증 시작 date
+
+    private Date periodEndDate; // 인증 마지막 date
+
+    private Date expiredDay; // 참가 신청 마지막 date
 
 }
--- src/main/java/com/example/developers/domain/Challenge.java
+++ src/main/java/com/example/developers/domain/Challenge.java
@@ -1,6 +1,7 @@
 package com.example.developers.domain;
 
 import com.example.developers.DTO.ChallengeDTO;
+import com.google.firebase.auth.FirebaseToken;
 import lombok.*;
 
 import javax.persistence.*;
@@ -22,32 +23,55 @@ public class Challenge {
     private Long id;
 
     @Column
-    private String goal;
+    private String topic; // 주제
 
-    @Column(name = "auth_method")
-    private String authMethod;
+    @Column(name = "how_proof")
+    private String howProof; // 인증 방법
 
-    @Column(name = "expected_effects")
-    private String expectedEffects;
+    @Column(name = "expected_results")
+    private String expectedResults; // 기대 효과
 
-    @Column
-    private String caution;
+    @Column(name = "please_note")
+    private String pleaseNote; // 주의 사항
 
-    @Column
-    private Date date;
+    @Column(name = "collect_proof")
+    private String collectProof; // 올바른 예시 이미지
+
+    @Column(name = "period_start_date")
+    private Date periodStartDate; // 인증 시작 date
+
+    @Column(name = "period_end_date")
+    private Date periodEndDate; // 인증 마지막 date
+
+    @Column(name = "expired_day")
+    private Date expiredDay; // 참가 신청 마지막 date
 
     @OneToMany(mappedBy = "challenge", cascade = CascadeType.PERSIST, orphanRemoval = true)
     private List<MemberChallenge> memberChallenges = new ArrayList<>();
 
     public ChallengeDTO toDTO() {
         return ChallengeDTO.builder()
-                .id(id)
-                .authMethod(authMethod)
-                .caution(caution)
-                .date(date)
-                .expectedEffects(expectedEffects)
-                .goal(goal)
+                .topic(topic)
+                .howProof(howProof)
+                .expectedResults(expectedResults)
+                .pleaseNote(pleaseNote)
+                .collectProof(collectProof)
+                .periodStartDate(periodStartDate)
+                .periodEndDate(periodEndDate)
+                .expiredDay(expiredDay)
                 .build();
     }
+
+    public void update(ChallengeDTO challengeDTO) {
+        this.topic = challengeDTO.getTopic();
+        this.howProof = challengeDTO.getHowProof();
+        this.expectedResults = challengeDTO.getExpectedResults();
+        this.pleaseNote = challengeDTO.getPleaseNote();
+        this.collectProof = challengeDTO.getCollectProof();
+        this.periodStartDate = challengeDTO.getPeriodStartDate();
+        this.periodEndDate = challengeDTO.getPeriodEndDate();
+        this.expiredDay = challengeDTO.getExpiredDay();
+    }
+
 
 }
